03a58f6a35692ad7be5aa89f629c4f904faaefb1
gaurav <gauravb_007@yahoo.com>
2022-09-06 20:13:36 +0530

    bug fixes

--- linked_list/doubly_linked_list.go
+++ linked_list/doubly_linked_list.go
@@ -12,24 +12,25 @@ type DLL struct {
 }
 
 // Adds node to the rear of the list
-func (list *DLL) addNode(value int) *DLLNode {
-	newNode := &DLLNode{
-		val: value,
-	}
-	if list.front == nil {
+func (list *DLL) addNode(newNode *DLLNode) {
+	if list.front == nil && list.rear == nil {
+		// empty list
 		list.front = newNode
 		list.rear = newNode
 	} else {
+		newNode.prev = list.rear
 		list.rear.next = newNode
 		list.rear = newNode
 	}
-
-	return newNode
 }
 
 // Removes node from the list
 func (list *DLL) removeNode(node *DLLNode) {
-	if node == list.front {
+	if node == list.front && node == list.rear {
+		// single node in the list
+		list.front = nil
+		list.rear = nil
+	} else if node == list.front {
 		list.front = list.front.next
 	} else if node == list.rear {
 		list.rear = list.rear.prev
